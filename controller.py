477cdd267189a2dc12514f30886e02ed0c71a833
Taseen18 <taseen.rahman7@gmail.com>
2023-11-05 00:08:46 +0000

    Update controller.py
    Now has a discovery function built in. It lets you control multiple devices that have been discovered or using a custom IP.

--- controller.py
+++ controller.py
@@ -1,25 +1,70 @@
 import socket
 import json
 import time
+import threading
+import sys
 
 
 multicast_ip = "239.255.255.250"
-multicast_discover = 4001
+multicast_send = 4001
 multicast_receive = 4002
 
-# Define the device's IP address and port
-device_port = 4003  # Replace with the appropriate port for control
+device_port = 4003 
 running = True
-
+responses = [] # Holds dictionaries containing all details of each response
+completion_event = threading.Event() # signals the completion of the discovery function
 
 def discoverDevices():
-    sendDiscoveryPacket()
+    threading.Thread(target=scanResponses).start()
+    threading.Thread(target=sendDiscoveryPacket).start()
+    message = "Discovering Devices"
+    threading.Thread(target=loadingMessage, args=(message,)).start()
 
+    time.sleep(8)
+    return responses
+
+def scanResponses():
+    listen_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    listen_socket.bind(('', multicast_receive))
+    listen_socket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, socket.inet_aton(multicast_ip) + socket.inet_aton("0.0.0.0"))
+    listen_socket.settimeout(5) # Sets the discovery time
+
+    lock = threading.Lock()
+
+    #Processes each response and adds it to the list
+    def process_response(data, addr):
+        received_message = data.decode('utf-8')
+        try:
+            received_json = json.loads(received_message)
+            device_info = {
+                'ip': addr[0],  # Extract the IP address from the source address tuple
+                'device': received_json.get("msg", {}).get("data", {}).get("device")
+            }
+            if device_info['device']:
+                with lock:
+                    responses.append(device_info)
+                    #print(f"Received response from {addr[0]}: {device_info}")      # uncomment to print as soon as a response is received
+        except json.JSONDecodeError:
+            print("Received non-JSON message:", received_message)
+
+    # Start listening for responses
+    try:
+        while True:
+            data, addr = listen_socket.recvfrom(10240)
+            threading.Thread(target=process_response, args=(data, addr)).start() # Starts a thread once a response is received to process it concurrently
+    except socket.timeout:
+        pass
+        completion_event.set()
+        return responses
+
+    listen_socket.close()
+
+    
 def sendDiscoveryPacket():
     discovery_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    # Define the "cmd: scan" command for discovery
-    cmd_scan = {
+    command = {
         "msg": {
             "cmd": "scan",
             "data": {
@@ -28,9 +73,20 @@ def sendDiscoveryPacket():
         }
     }
 
-    cmd_scan_json = json.dumps(cmd_scan)
-    discovery_socket.sendto(cmd_scan_json.encode('utf-8'), (multicast_ip, multicast_port))
+    command_json = json.dumps(command)
+    discovery_socket.sendto(command_json.encode('utf-8'), (multicast_ip, multicast_send))
     discovery_socket.close()
+
+def loadingMessage(message):
+    while not completion_event.is_set():
+        for char in "-\|/":
+            print(f" {message} {char}", end="\r")
+            time.sleep(0.2)
+            print(" " * 25, end="\r")
+
+    print(" Loading Complete", end="\r")
+    time.sleep(2)  # Display the completion message for 2 seconds
+    print(" " * 25, end="\r")  # Clear the line again
         
 def powerOn(device_ip):
     command = {
@@ -43,7 +99,7 @@ def powerOn(device_ip):
     }
     sendCommand(command, device_ip)
     time.sleep(1)
-    sync()
+    #sync()
 
 def powerOff(device_ip):
     command = {
@@ -56,7 +112,7 @@ def powerOff(device_ip):
     }
     sendCommand(command, device_ip)
     time.sleep(1)
-    sync()
+    #sync()
 
 def adjustBrightness(device_ip):
     brightness = -1
@@ -73,12 +129,12 @@ def adjustBrightness(device_ip):
     }
     sendCommand(command, device_ip)
     time.sleep(1)
-    sync()
+    #sync()
 
 def changeColour():
     pass
 
-def sync():
+def sync(device_ip):
     command = {
         "msg": {
             "cmd": "devStatus",
@@ -102,22 +158,72 @@ def sendCommand(command, device_ip):
     controller_socket.close()
 
 
+def getDeviceIP(index):
+    device = responses[index]
+    if device:
+        ip = device.get('ip', 'N/A')
+        return ip
+    return None
 
-  
-while running:
-    device_ip = "192.168.0.40"
-    print("\n~~CONTROLS~~\n1. Power On\n2. Power Off\n3. Adjust Brightness\n4. Sync\n5. (quit)")
-    selection = str(input("--> "))
-    if selection == '1':
-        powerOn(device_ip)
-    elif selection == '2':
-        powerOff(device_ip)
-    elif selection == '3':
-        adjustBrightness(device_ip)
-    elif selection == '4':
-        sync(device_ip)
-    elif selection == '5' or selection.lower() == 'quit':
-        running = False
+def getDeviceMac(index):
+    device = responses[index]
+    if device:
+        mac = device.get('device', 'N/A')
+        return mac
+    return None
+    
+
+def displayDevices():
+    if len(responses) <= 0:
+        print("\nNo Devices Found")
     else:
-        pass
+        counter = 1
+        for device in responses:
+            print("\nDevice", counter)
+            print("IP", getDeviceIP(counter-1))
+            print("MAC", getDeviceMac(counter-1))
+            counter += 1
+
+
+def controlMenu(device_ip):
+    while True:
+        print("\n~~CONTROLS~~\n1. Power On\n2. Power Off\n3. Adjust Brightness\n4. Sync\n5. Change Device")
+        selection = str(input("--> "))
+        if selection == '1':
+            powerOn(device_ip)
+        elif selection == '2':
+            powerOff(device_ip)
+        elif selection == '3':
+            adjustBrightness(device_ip)
+        elif selection == '4':
+            sync(device_ip)
+        elif selection == '5':
+            break
+        else:
+            pass
+
+def deviceMenu():
+    if len(responses) > 0:
+        displayDevices()
+        while True:
+            print("\n~~SELECT DEVICE~~")
+            for counter in range(len(responses)):
+                print(str(counter+1) + ". Device " + str(counter+1))
+            print(str(len(responses)+1) + ". Enter Custom IP")
+            print(str(len(responses)+2) + ". (quit)")
+            selectedDevice = input("--> ")
+            if int(selectedDevice) > 0 and int(selectedDevice) <= len(responses):
+                device_ip = getDeviceIP(int(selectedDevice)-1)
+                controlMenu(device_ip)
+            elif int(selectedDevice) == len(responses)+1:
+                device_ip = str(input("\n Enter Custom IP: "))
+                controlMenu(device_ip)
+            elif int(selectedDevice) == len(responses)+2 or selectedDevice.lower() == 'quit':
+                break
+        
+
+print("\nDevices: ", discoverDevices())
+#print(discoverDevices())
+deviceMenu()
+
 
